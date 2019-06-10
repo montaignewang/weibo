c38e038cb5fd85ec37946971b99d532991238dbd
montaignewang <276993997@qq.com>
2019-06-10 10:14:21 +0800

    登陆视图修改

--- login/views.py
+++ login/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
-
+from . import models
 # Create your views here.
 
 def index(request):
@@ -8,8 +8,29 @@ def index(request):
     return render(request,'login/index.html')
 
 def login(request):
-    pass
-    return render(request, 'login/login.html')
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        message = '请检查填写的内容！'
+        if username.strip() and password:  # 确保用户名和密码都不为空
+        # 用户名字符合法性验证
+        # 密码长度验证
+        # 更多的其它验证.....
+            try:
+                user = models.User.objects.get(name=username)
+            except:
+                message = '用户不存在！'
+                return render(request,'login/login.html', {'message': message})
+            if user.password == password:
+                print(username,password)
+                return redirect('/index/')
+            else:
+                message = '密码不正确！'
+                return render(request, 'login/login.html', {'message': message})
+        else:
+            return render(request,'login/login.html',{'message': message})
+    return render(request,'login/login.html')
+
 
 def register(request):
     pass
