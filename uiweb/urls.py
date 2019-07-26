88fe7fdec92b575a8c1e0cf78cd261207332a634
montaignewang <276993997@qq.com>
2019-07-26 20:48:30 +0800

    增加点赞及点赞计数功能，利用ajax实现

--- uiweb/urls.py
+++ uiweb/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     #path('index/',views.index),
     path('<str:username>/',views.userpage,name='userpage'),  #显示用户发过的微博页面
     path('',views.homepage,name='homepage'), #显示用户关注好友发布的微博
+    path('ajax/add_zan',views.add_zan,name='homepage'),
     #path('comment/<int:weibo_id>/',views.comment,name='comment'),
 ]
